aca7b0b24321d345a04c74f258ed8fbaacbcb1c4
taylodl <donald_l_taylor_jr@yahoo.com>
2024-02-09 16:23:12 -0500

    Storage is now in memcached

--- DataAPI/LoanAppCache.py
+++ DataAPI/LoanAppCache.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 import json
+import memcache
 
 # Initialize Flask app
 app = Flask(__name__)
 
-# Initialize dictionary cache
-cache = {}
+# Create a memcached client to use
+memcached_client = memcache.Client(['memcached:11211'])
 
 @app.route('/test', methods=['GET'])
 def test():
@@ -25,7 +26,7 @@ def store():
     key = f"{months}_{amount}_{apr}"
     json_data = request.json
     cache_data = json.dumps(json_data)
-    cache.update({key: cache_data})
+    memcached_client.set(key, cache_data)
     return jsonify({'message': 'Data stored successfully'}), 200
 
 # Endpoint to retrieve data from memcached
@@ -36,8 +37,8 @@ def retrieve():
     amount = request.args.get('amount')
     apr = request.args.get('apr')
     key = f"{months}_{amount}_{apr}"
-    data = cache.get(key)
-    if data:
+    data = memcached_client.get(key)
+    if data is not None:
         return jsonify(json.loads(data)), 200
     else:
         return jsonify({'error': 'Data not found'}), 404
